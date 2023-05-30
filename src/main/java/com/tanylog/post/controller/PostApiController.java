7439db36734a27e4e9a72e311d94fbec165c796b
ByeoungJoon Jeon <juni8453@naver.com>
2023-05-30 22:00:40 +0900

    게시글 단건조회 API 추가

--- src/main/java/com/tanylog/post/controller/PostApiController.java
+++ src/main/java/com/tanylog/post/controller/PostApiController.java
@@ -1,11 +1,13 @@
 package com.tanylog.post.controller;
 
 import com.tanylog.post.controller.request.PostCreate;
+import com.tanylog.post.controller.response.PostRead;
 import com.tanylog.post.service.PostService;
-import java.util.Map;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,11 @@ public class PostApiController {
   public void post(@RequestBody @Valid PostCreate postCreate) {
     log.info("postRequest={}", postCreate.toString());
     postService.write(postCreate);
+  }
+
+  @GetMapping("/api/posts/{postId}")
+  public PostRead read(@PathVariable Long postId) {
+    log.info("postId={}", postId);
+    return postService.read(postId);
   }
 }
--- src/main/java/com/tanylog/post/service/PostService.java
+++ src/main/java/com/tanylog/post/service/PostService.java
@@ -1,6 +1,7 @@
 package com.tanylog.post.service;
 
 import com.tanylog.post.controller.request.PostCreate;
+import com.tanylog.post.controller.response.PostRead;
 import com.tanylog.post.domain.Post;
 import com.tanylog.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +15,22 @@ public class PostService {
 
   private final PostRepository postRepository;
 
-  public void write(PostCreate postCreate) {
+  public void write(PostCreate createPost) {
     Post post = Post.builder()
-        .title(postCreate.getTitle())
-        .content(postCreate.getContent())
+        .title(createPost.getTitle())
+        .content(createPost.getContent())
         .build();
 
     postRepository.save(post);
+  }
+
+  public PostRead read(Long postId) {
+    Post findPost = postRepository.findById(postId)
+        .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 게시글입니다."));
+
+    return PostRead.builder()
+        .title(findPost.getTitle())
+        .content(findPost.getContent())
+        .build();
   }
 }
