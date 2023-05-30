3f901014a47e2004de75168e9c8594b123be0587
ByeoungJoon Jeon <juni8453@naver.com>
2023-05-30 17:22:00 +0900

    게시글 생성 관련 DTO 추가

---
+++ src/main/java/com/tanylog/post/controller/request/PostCreate.java
@@ -0,0 +1,25 @@
+package com.tanylog.post.controller.request;
+
+import javax.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class PostCreate {
+
+  @NotBlank(message = "title 을 입력해주세요.")
+  private String title;
+
+  @NotBlank(message = "content 를 입력해주세요.")
+  private String content;
+
+  @Builder
+  public PostCreate(String title, String content) {
+    this.title = title;
+    this.content = content;
+  }
+}
