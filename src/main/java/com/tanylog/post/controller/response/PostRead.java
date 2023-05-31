3bed64c62fd0729fe6dfd1b233284a8a8ebf42a1
ByeoungJoon Jeon <juni8453@naver.com>
2023-05-31 16:47:15 +0900

    PostRead 객체 필드 값 추가 - Long id 추가

--- src/main/java/com/tanylog/post/controller/response/PostRead.java
+++ src/main/java/com/tanylog/post/controller/response/PostRead.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 @Getter
 public class PostRead {
 
+  private final Long id;
   private final String title;
   private final String content;
 
   @Builder
-  public PostRead(String title, String content) {
+  public PostRead(Long id, String title, String content) {
+    this.id = id;
     this.title = title;
     this.content = content;
   }
